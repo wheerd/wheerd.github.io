9adf3edb38751ec166603d58515761435c070aca
Wheerd <admin@wheerd.de>
2016-02-25 14:47:35 +0100

    Added state toggeling to cards.

--- main.js
+++ main.js
@@ -28,8 +28,45 @@
       div2.innerHTML = currentWords[i]
       div.appendChild(div2)
       container.appendChild(div)
+
+      div2.onclick = stateToggler(i)
+    }
+
+    resetState()
+  }
+
+  function refreshStates () {
+    var state = JSON.parse(localStorage.state)
+    var container = document.getElementById('cardContainer')
+    var cards = container.childNodes
+    for (var i = 0; i < 25; i++) {
+      cards[i].className = 'card card-' + i + ' state-' + state[i]
     }
   }
 
+  function resetState () {
+    var state = _.times(25, _.constant(0))
+    localStorage.state = JSON.stringify(state)
+
+    refreshStates()
+  }
+
+  function stateToggler (i) {
+    return function () {
+      toggleState(i)
+    }
+  }
+
+  function toggleState (i) {
+    var state = JSON.parse(localStorage.state)
+    state[i] = (state[i] + 1) % 5
+    localStorage.state = JSON.stringify(state)
+
+    console.log(state)
+
+    refreshStates()
+  }
+
   window.generateWords = generateWords
+  window.resetState = resetState
 })(window.$, window._, window.localStorage)
